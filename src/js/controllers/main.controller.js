94d036ea5b5ff45246a0de1e2e637a83f6523b5f
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-02-20 14:03:56 +0000

    wrote MainCtrl

--- src/js/controllers/main.controller.js
+++ src/js/controllers/main.controller.js
@@ -3,3 +3,20 @@ angular
 .controller('MainCtrl', MainCtrl)
 
 MainCtrl.$inject =['$rootScope', '$state' 'CurrentUserService']
+function MainCtrl($rootScope, CurrentUserService, $state)
+const vm = this;
+vm.user = CurrentUserService.getUser();
+$rootScope.$on("loggedIn", () => {
+  vm.user = CurrentUserService.getUser();
+  $state.go("auctionsIndex");
+});
+
+vm.logout = () => {
+  event.preventDefault();
+  CurrentUserService.clearUser();
+};
+
+$rootScope.$on("loggedOut", () => {
+  vm.user = null;
+  $state.go("home");});
+  
