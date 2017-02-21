03c6c31064301018c9a382242f753d79343eca3f
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-02-21 19:40:39 +0000

    made intial controller for bgTests index with a state change to login if there is no Token

--- src/js/controllers/bgTests/index.js
+++ src/js/controllers/bgTests/index.js
@@ -1,3 +1,12 @@
 angular
 .module('Diabetus')
 .controller('bgTestsCtrl', bgTestsCtrl);
+
+ bgTestsCtrl.$inject = ['BgTests','TokenService', '$state'];
+ function bgTestsCtrl(BgTests){
+    const vm = this;
+    vm.bgTests = BgTests.query();
+    if (!TokenService.getToken()){
+  $state.go('login');
+}
+ }
