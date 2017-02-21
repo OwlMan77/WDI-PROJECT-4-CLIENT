05e7c3b8e488e0005aba2cdb8b76d0712d3e5bc9
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-02-21 19:49:50 +0000

    added update controller for user

---
+++ src/js/controllers/users/update.controller.js
@@ -0,0 +1,25 @@
+angular
+  .module('Diabetus')
+  .controller('usersUpdateCtrl', usersUpdateCtrl);
+
+usersUpdateCtrl.$inject = ['User', '$state', '$stateParams'];
+function usersUpdateCtrl(User, $state, $stateParams) {
+  const vm = this;
+  User
+  .get($stateParams)
+  .$promise
+  .then(response => {
+    vm.user = response;
+  });
+
+
+  vm.update = () => {
+    User
+    .update($stateParams, { user: vm.user })
+    .$promise
+    .then(data => {
+      //need to put stateparamas to get user back to page as we need the id for the revelant page.
+      $state.go('usersShow', $stateParams);
+    });
+  };
+}
