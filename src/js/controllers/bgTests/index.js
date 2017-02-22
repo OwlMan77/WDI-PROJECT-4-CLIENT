7e1f0bf124417aa377f3a0189713c8beb1157a3f
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-02-22 15:11:50 +0000

    successfully added ng repeat on user medications, still trying to get bgTests to work

--- src/js/controllers/bgTests/index.js
+++ src/js/controllers/bgTests/index.js
@@ -2,8 +2,8 @@ angular
 .module('Diabetus')
 .controller('bgTestsIndexCtrl', bgTestsIndexCtrl);
 
- bgTestsIndexCtrl.$inject = ['Bg_test','TokenService', '$state'];
- function bgTestsIndexCtrl(Bg_test, TokenService, $state){
+ bgTestsIndexCtrl.$inject = ['BgTest','TokenService', '$state'];
+ function bgTestsIndexCtrl(BgTest, TokenService, $state){
     const vm = this;
     vm.bg_Tests = Bg_Tests.query();
     if (!TokenService.getToken()){
